5849add09634df4baa19a9963e7d1cf013cfb012
Dick Wall <dick@nospam.com>
2013-09-23 16:19:17 -0700

    Updated to demonstrate phantom types.

--- src/main/scala/FakeDb.scala
+++ src/main/scala/FakeDb.scala
@@ -5,18 +5,20 @@ import collection.mutable
 object FakeDb {
     private[this] val objects: mutable.Map[String, Persistable] = mutable.HashMap.empty
     
-    def save(p: Persistable): Unit = {
+    def save[P <: Persistable](p: P with NotYetPersisted): P with Persisted = {
         require(!objects.contains(p.id), "That ID is already in use")
         objects(p.id) = p
+        p.asInstanceOf[P with Persisted]
     }
     
-    def update(p: Persistable): Unit = {
+    def update[P <: Persistable with Persisted](p: P): P = {
         require(objects.contains(p.id), "No such record to update")
         objects(p.id) = p
+        p
     }
     
-    def find[A <: Persistable](id: String): A =
-        objects(id).asInstanceOf[A]
+    def find[A <: Persistable](id: String): A with Persisted =
+        objects(id).asInstanceOf[A with Persisted]
 }
 
 abstract class Persistable {
@@ -27,3 +29,11 @@ trait Persisted extends Persistable
 trait NotYetPersisted extends Persistable
 
 case class Person(id: String, first: String, last: String, age: Int) extends Persistable
+
+object Person {
+    def create(id: String, first: String, last: String, age: Int): Person with NotYetPersisted = 
+        new Person(id, first, last, age) with NotYetPersisted
+    def updated(p: Person with Persisted)(id: String = p.id, first: String = p.first, last: String = p.last, age: Int = p.age): Person with Persisted = {
+        new Person(id, first, last, age) with Persisted
+    }
+}
